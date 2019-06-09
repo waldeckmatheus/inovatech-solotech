42fc19d008cd2baab1f69cce290bb0d738bc6af5
Waldeck Matheus Belo <waldeckmatheus@gmail.com>
2019-06-09 17:21:59 -0400

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,67 @@
+
+
+var soloTechStorage = localStorage;
+document.addEventListener("click", function(){
+	console.log("Hello world!");
+});	
+
+var historico = JSON.parse(soloTechStorage.getItem("historico"));
+if (historico == null)
+	historico = [];
+
+var equipe= "Adriana Karla;\nBruno Lopes;\nChristian Rodrigues;\nElilson Leal;\nFelipe do Carmo;\nJulyson Batista;\nKeneson Liniker;\nLuciano Cabral;\nLutizemberg Oliveira;\nWaldeck Belo;";
+
+let equipeElement = document.getElementById("equipe");
+let mensagemElement = document.getElementById("mensagem");
+let yl69dataElement = document.getElementById("yl69data");
+
+ajaxMain();
+informarSePlantaPrecisaAgua(mensagemElement,yl69dataElement);
+setarDadosEquipe(equipeElement);
+adicionarEventos(equipeElement);
+
+function ajaxMain() {
+	var r = new XMLHttpRequest(); r.open("GET", "http://solotech/resources/main.html", true); 
+	r.onreadystatechange = function () { 
+	if (r.readyState != 4 || r.status != 200) 
+		return; 
+
+		var currentInterval = setInterval(()=>{
+			let conteudoElement = document.getElementById("conteudo");
+			conteudoElement.innerHTML = r.responseText;
+			clearInterval(currentInterval);
+		},500);
+		console.log("Response: ",r.responseText);
+	}; 
+
+	r.send();
+}
+
+function informarSePlantaPrecisaAgua(mensagemEl,yl69dataEl){
+	let msg = null;
+	if (yl69dataElement.value < 750)
+		msg = "A planta não precisa de água.";
+	else
+		msg = "A planta precisa de água.";
+	mensagemElement.innerText = msg;
+	let registro = [new Date().toLocaleString("pt-BR",{timeZone:'America/Manaus'}+";"), msg+" ("+yl69dataElement.value+");"];
+	historico.push(registro);
+	soloTechStorage.setItem("historico", JSON.stringify(historico));
+	console.log("Historico: ",historico);
+}
+function setarDadosEquipe(equipeElement){
+	equipeElement.value = equipe;
+	equipeElement.style.height = (equipeElement.scrollHeight)+"px";
+}
+function adicionarEventos(equipeElement){
+	equipeElement.addEventListener("mouseover", function(){
+		alert("Equipe: \n"+equipe.replace(new RegExp(";",'g')," "));
+
+		let strHist = "";
+		for (let i=0;i<historico.length;i++){
+			strHist+=historico[i][0].concat(": ").concat(historico[i][1]);
+			strHist+="\n";
+		}
+		alert(strHist);
+	});
+}
